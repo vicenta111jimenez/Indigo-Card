78565486b3b5837368386c3b9167dcd773327374
vicenta111jimenez <vicenta111jimenez@outlook.com>
2024-08-30 17:48:29 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipt</h2>
+      {/* Add receipt details here */}
+    </div>
+  );
+}
+
+export default Receipt;

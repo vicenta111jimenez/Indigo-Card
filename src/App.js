718410a259f0cbb65560a22515aaee8716587f40
vicenta111jimenez <vicenta111jimenez@outlook.com>
2024-08-30 17:41:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import Receipt from './components/Receipt';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ExpenseForm />
+      <ExpenseList />
+      <Receipt />
+    </div>
+  );
+}
+
+export default App;
